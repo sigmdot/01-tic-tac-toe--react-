cb7efcc6da744d00999c66dc65138ee15ff3144d
Sebastián García <sebastian.garcia.m01@mail.pucv.cl>
2020-11-24 16:23:54 -0300

    Inicio proyecto

---
+++ src/Board.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+
+import Square from './Square'
+
+class Board extends Component {
+    state = {
+        squares: ['', '', '', '', '', '', '', '', ''],
+        xIsNext: true
+    }
+
+    handleClick = number => () => {
+
+        if (this.state.squares[number] === '') {
+            const newSquares = [... this.state.squares];
+
+            newSquares[number] = this.state.xIsNext ? 'X' : 'O';
+
+            this.setState({
+                squares: newSquares,
+                xIsNext: !this.state.xIsNext
+            });
+        }
+    }
+
+    render() {
+        const { squares, xIsNext } = this.state
+        return (
+
+            <div className='Board'>
+                <h1>Next step: {xIsNext ? 'X' : 'O'}</h1>
+                <div className='Row'>
+                    <Square value={squares[0]} onClick={this.handleClick(0)} />
+                    <Square value={squares[1]} onClick={this.handleClick(1)} />
+                    <Square value={squares[2]} onClick={this.handleClick(2)} />
+                </div>
+                <div className='Row'>
+                    <Square value={squares[3]} onClick={this.handleClick(3)} />
+                    <Square value={squares[4]} onClick={this.handleClick(4)} />
+                    <Square value={squares[5]} onClick={this.handleClick(5)} />
+                </div>
+                <div className='Row'>
+                    <Square value={squares[6]} onClick={this.handleClick(6)} />
+                    <Square value={squares[7]} onClick={this.handleClick(7)} />
+                    <Square value={squares[8]} onClick={this.handleClick(8)} />
+                </div>
+            </div>
+
+        );
+    }
+}
+
+export default Board;
