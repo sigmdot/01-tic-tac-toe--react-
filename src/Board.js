ae3112323345b9a6537f3d09bc59d81e09bea32a
Sebastián García <sebastian.garcia.m01@mail.pucv.cl>
2020-11-24 16:38:12 -0300

    Paso 2

--- src/Board.js
+++ src/Board.js
@@ -2,6 +2,36 @@ import React, { Component } from 'react';
 
 import Square from './Square'
 
+const calcularGanador = squares => {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+
+        [0, 4, 8],
+        [2, 4, 6]
+    ]
+
+    for (let i = 0; i < lines.length; i++) {
+        const[a,b,c] = lines[i]
+
+        if ( 
+            squares[a] &&
+            squares[a] === squares[b] &&
+            squares[a] === squares[c]
+        ) {
+            return squares[a]
+        }
+    }
+
+    return ''
+
+}
+
 class Board extends Component {
     state = {
         squares: ['', '', '', '', '', '', '', '', ''],
@@ -10,24 +40,35 @@ class Board extends Component {
 
     handleClick = number => () => {
 
-        if (this.state.squares[number] === '') {
-            const newSquares = [... this.state.squares];
-
-            newSquares[number] = this.state.xIsNext ? 'X' : 'O';
-
-            this.setState({
-                squares: newSquares,
-                xIsNext: !this.state.xIsNext
-            });
+        if (this.state.squares[number] || calcularGanador(this.state.squares)) {
+            return
         }
+        const newSquares = [... this.state.squares];
+
+        newSquares[number] = this.state.xIsNext ? 'X' : 'O';
+
+        this.setState({
+            squares: newSquares,
+            xIsNext: !this.state.xIsNext
+        });
     }
 
     render() {
         const { squares, xIsNext } = this.state
+
+        const winner = calcularGanador(this.state.squares)
+
+        let status
+
+        if(winner){
+            status =  `Winner: ${winner}`
+        } else {
+            status =  `Next step: ${xIsNext ? 'X' : 'O' }`
+        }
         return (
 
             <div className='Board'>
-                <h1>Next step: {xIsNext ? 'X' : 'O'}</h1>
+                <h1>{status}</h1>
                 <div className='Row'>
                     <Square value={squares[0]} onClick={this.handleClick(0)} />
                     <Square value={squares[1]} onClick={this.handleClick(1)} />
@@ -43,6 +84,8 @@ class Board extends Component {
                     <Square value={squares[7]} onClick={this.handleClick(7)} />
                     <Square value={squares[8]} onClick={this.handleClick(8)} />
                 </div>
+
+                <button onClick={this.handleReset} className='Reset'>Reiniciar</button>
             </div>
 
         );
